6977ef29a9c9f417c14c672d74687541f0200430
Anuj Rajput <anujrajput04@gmail.com>
2020-07-30 12:02:27 +0530

    Added Project 2: GuessTheFlag

---
+++ UIKit/Project2/GuessTheFlag/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Anuj Rajput on 30/07/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
