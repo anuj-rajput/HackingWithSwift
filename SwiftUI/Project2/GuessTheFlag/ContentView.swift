1762e6d2654aad94e295db049b47a36d7ad09862
Anuj Rajput <anujrajput04@gmail.com>
2020-07-31 13:27:52 +0530

    Renamed SwiftUI Project1 and created SwiftUI Project 2: GuessTheFlag

---
+++ SwiftUI/Project2/GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Anuj Rajput on 31/07/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
