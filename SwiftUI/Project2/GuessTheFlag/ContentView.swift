02285e4ffe1fba331f79b9f8fd14b278b1614488
Anuj Rajput <anujrajput04@gmail.com>
2020-08-01 23:42:37 +0530

    Added scoring

--- SwiftUI/Project2/GuessTheFlag/ContentView.swift
+++ SwiftUI/Project2/GuessTheFlag/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
     
     var body: some View {
         ZStack {
@@ -33,7 +34,7 @@ struct ContentView: View {
                 
                 ForEach (0 ..< 3) { number in
                     Button(action: {
-                        // flag was tapped
+                        self.flagTapped(number)
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
@@ -47,7 +48,7 @@ struct ContentView: View {
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
                 })
         }
@@ -56,8 +57,10 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
         } else {
             scoreTitle = "Wrong"
+            score -= 1
         }
         
         showingScore = true
