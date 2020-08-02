a9d0fc43188040b61a790a761bd9314dc2b9a687
Anuj Rajput <anujrajput04@gmail.com>
2020-08-02 12:47:26 +0530

    Created UIKit Project 3: Social Media by copying files from Project 1

---
+++ UIKit/Project3/SocialMedia/DetailViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DetailViewController.swift
+//  SocialMedia
+//
+//  Created by Anuj Rajput on 02/08/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var currentPicture: Int = 0
+    var totalPicture: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Picture \(currentPicture) of \(totalPicture)"
+        navigationItem.largeTitleDisplayMode = .never
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
