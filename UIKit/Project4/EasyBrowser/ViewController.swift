5cac3b1482313b15a1921230f9bc6936ae91ff2c
Anuj Rajput <anujrajput04@gmail.com>
2020-08-10 23:17:29 +0530

    Challenge: Two new toolbar items with the titles Back and Forward. Disables when there is not next or previous page. Progress view hides when page is loaded

--- UIKit/Project4/EasyBrowser/ViewController.swift
+++ UIKit/Project4/EasyBrowser/ViewController.swift
@@ -12,6 +12,8 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
     var progressView: UIProgressView!
+    var goBack: UIBarButtonItem!
+    var goForward: UIBarButtonItem!
     var websites = ["apple.com", "anujrajput.com"]
     
     override func loadView() {
@@ -28,14 +30,22 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         
+        goBack = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
+        goBack.isEnabled = false
+        goForward = UIBarButtonItem(title: "Forward", style: .plain, target: webView, action: #selector(webView.goForward))
+        goForward.isEnabled = false
+        
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
+        progressView.isHidden = true
         let progressButton = UIBarButtonItem(customView: progressView)
         
-        toolbarItems = [progressButton, spacer, refresh]
+        toolbarItems = [progressButton, spacer, goBack, goForward, refresh]
         navigationController?.isToolbarHidden = false
         
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack), options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward), options: .new, context: nil)
         
         let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
@@ -61,8 +71,13 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.load(URLRequest(url: url))
     }
     
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        progressView.isHidden = false
+    }
+    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+        progressView.isHidden = true
     }
     
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
@@ -87,6 +102,14 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == "estimatedProgress" {
             progressView.progress = Float(webView.estimatedProgress)
+        }
+        
+        if keyPath == "canGoBack" {
+            goBack.isEnabled = (object as! WKWebView).canGoBack
+        }
+        
+        if keyPath == "canGoForward" {
+            goForward.isEnabled = (object as! WKWebView).canGoForward
         }
     }
 
