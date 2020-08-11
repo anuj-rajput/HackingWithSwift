10bd5b54fc97f5799cb0bc0a0a1c0018eee585c4
Anuj Rajput <anujrajput04@gmail.com>
2020-08-11 19:04:58 +0530

    Project 5: WordScramble

---
+++ UIKit/Project5/WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Anuj Rajput on 11/08/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
