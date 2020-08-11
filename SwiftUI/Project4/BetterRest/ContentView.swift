9ef3186467af9d47b27138ba1d2e3fe774860163
Anuj Rajput <anujrajput04@gmail.com>
2020-08-11 13:00:02 +0530

    Date and Date Pickers

--- SwiftUI/Project4/BetterRest/ContentView.swift
+++ SwiftUI/Project4/BetterRest/ContentView.swift
@@ -8,13 +8,16 @@
 
 import SwiftUI
 
+let now = Date()
+let tomorrow = Date().addingTimeInterval(86400)
+let range = now ... tomorrow
+
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
-    
+    @State private var wakeUp = Date()
+        
     var body: some View {
-        Stepper(value: $sleepAmount, in: 4 ... 12, step: 0.25) {
-            Text("\(sleepAmount, specifier: "%g") hours")
-        }
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date()..., displayedComponents: .hourAndMinute)
+            .labelsHidden()
     }
 }
 
