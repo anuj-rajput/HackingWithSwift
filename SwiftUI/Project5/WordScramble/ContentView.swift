57c4309d9bf0dc174f79df41f7a22f8655b07389
Anuj Rajput <anujrajput04@gmail.com>
2020-08-13 17:02:29 +0530

    Project 5: WordScramble Adding to a list of words

---
+++ SwiftUI/Project5/WordScramble/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Anuj Rajput on 13/08/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .autocapitalization(.none)
+                
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+        }
+        .navigationBarTitle(rootWord)
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {
+            return
+        }
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
