319d45d99bcb16358bc9d34fb513b3253da721f1
Anuj Rajput <anujrajput04@gmail.com>
2020-08-13 15:32:06 +0530

    Challenge: Height of the labels equal to 1/5th of the main view, minus 10 for the spacing

--- UIKit/Project6b/AutoLayout/ViewController.swift
+++ UIKit/Project6b/AutoLayout/ViewController.swift
@@ -65,7 +65,8 @@ class ViewController: UIViewController {
             label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
             label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
             
-            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+//            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            label.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
             
             if let previous = previous {
                 label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
