dd7b40f2a7ea929be1620c988c9ab29877d8100a
Anuj Rajput <anujrajput04@gmail.com>
2020-08-14 18:46:21 +0530

    Parsing JSON using the Codable protocol

---
+++ UIKit/Project7/WhitehousePetitions/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  WhitehousePetitions
+//
+//  Created by Anuj Rajput on 14/08/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
