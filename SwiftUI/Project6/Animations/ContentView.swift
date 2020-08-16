a03183e481da660d5e80196d5ef18c990b95878e
Anuj Rajput <anujrajput04@gmail.com>
2020-08-16 14:21:43 +0530

    Animating gestures: onEnded smooth transititon

--- SwiftUI/Project6/Animations/ContentView.swift
+++ SwiftUI/Project6/Animations/ContentView.swift
@@ -13,18 +13,21 @@ struct ContentView: View {
     
     var body: some View {
         LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-        .frame(width: 300, height: 200)
-        .clipShape(RoundedRectangle(cornerRadius: 10))
-        .offset(dragAmount)
-        .gesture(
-            DragGesture()
-            .onChanged {
-                self.dragAmount = $0.translation
-            }
-            .onEnded { _ in
-                self.dragAmount = .zero
-            }
-        )
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged {
+                        self.dragAmount = $0.translation
+                }
+                .onEnded { _ in
+                    withAnimation(.spring()) {
+                        self.dragAmount = .zero
+                    }
+                }
+            )
+            .animation(.spring())
     }
 }
 
