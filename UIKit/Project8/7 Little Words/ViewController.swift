042348355729569796d9229c1666336a2e068f20
Anuj Rajput <anujrajput04@gmail.com>
2020-08-18 13:19:15 +0530

    Added Project 8: 7 Little Words

---
+++ UIKit/Project8/7 Little Words/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  7 Little Words
+//
+//  Created by Anuj Rajput on 18/08/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
