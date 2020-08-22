1a8b156122ca9c2052e53c7bac97d768913dae2a
Anuj Rajput <anujrajput04@gmail.com>
2020-08-22 19:00:26 +0530

    Challenge: Put loading images from Bundle in backgrounc

--- UIKit/Project9/Project1/StormViewer/ViewController.swift
+++ UIKit/Project9/Project1/StormViewer/ViewController.swift
@@ -17,19 +17,25 @@ class ViewController: UITableViewController {
         title = "Storm Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        let fileManager = FileManager.default
-        let path = Bundle.main.resourcePath!
-        let items = try! fileManager.contentsOfDirectory(atPath: path)
-        
-        for item in items {
-            if item.hasPrefix("nssl") {
-                // this is the picture to load!
-                pictures.append(item)
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            let fileManager = FileManager.default
+            let path = Bundle.main.resourcePath!
+            let items = try! fileManager.contentsOfDirectory(atPath: path)
+            
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    // this is the picture to load!
+                    self?.pictures.append(item)
+                }
             }
         }
         
         pictures.sort()
         print(pictures)
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.tableView.reloadData()
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
