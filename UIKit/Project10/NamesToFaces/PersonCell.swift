27afd3dc7e94af6cfe6b1edd4696dd1eee2cf68a
Anuj Rajput <anujrajput04@gmail.com>
2020-08-28 13:27:41 +0530

    Added Project 10, Designing UICollectionView cells

---
+++ UIKit/Project10/NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Anuj Rajput on 28/08/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
---
+++ UIKit/Project10/NamesToFaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Anuj Rajput on 28/08/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
