5daaa7b65e680909f35fc9b66b8b5e3e83760945
Anuj Rajput <anujrajput04@gmail.com>
2020-08-28 22:06:47 +0530

    Challenge: Added a second UIAlertController that gets shown when the user taps a picture, asking them whether they want to rename the person or delete them.

--- UIKit/Project10/NamesToFaces/ViewController.swift
+++ UIKit/Project10/NamesToFaces/ViewController.swift
@@ -74,17 +74,31 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
         
-        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
-        ac.addTextField()
+        let optionAc = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        optionAc.addAction(UIAlertAction(title: "Rename person", style: .default) { [weak self, weak optionAc] _ in
+            let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+            ac.addTextField()
+            
+            ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+                guard let newName = ac?.textFields?[0].text else { return }
+                person.name = newName
+                self?.collectionView.reloadData()
+            })
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            
+            optionAc?.dismiss(animated: true) { [weak self] in
+                self?.present(ac, animated: true)
+            }
+        })
         
-        ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
-            guard let newName = ac?.textFields?[0].text else { return }
-            person.name = newName
+        optionAc.addAction(UIAlertAction(title: "Delete", style: .destructive) { [weak self] _ in
+            self?.people.remove(at: indexPath.item)
             self?.collectionView.reloadData()
         })
         
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        present(ac, animated: true)
+        optionAc.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(optionAc, animated: true)
     }
 }
 
