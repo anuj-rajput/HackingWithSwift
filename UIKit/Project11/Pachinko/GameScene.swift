ab46ac9bc9e638fd5dbb43a37b35e538af6eed4f
Anuj Rajput <anujrajput04@gmail.com>
2020-08-29 21:59:18 +0530

    Challenge: Forcing Y value of new balls so they are near the top of the screen

--- UIKit/Project11/Pachinko/GameScene.swift
+++ UIKit/Project11/Pachinko/GameScene.swift
@@ -66,6 +66,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
+        guard location.y >= 700 else { return }
         
         let objects = nodes(at: location)
         
