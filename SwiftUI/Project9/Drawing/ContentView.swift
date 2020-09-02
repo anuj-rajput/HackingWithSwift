d7c1cc8ffb161c65980be9d9e2475089abb0198b
Anuj Rajput <anujrajput04@gmail.com>
2020-09-02 14:13:59 +0530

    Animating simple shapes with animatableData

--- SwiftUI/Project9/Drawing/ContentView.swift
+++ SwiftUI/Project9/Drawing/ContentView.swift
@@ -8,37 +8,38 @@
 
 import SwiftUI
 
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+}
+
 struct ContentView: View {
-    @State private var amount: CGFloat = 0.0
+    @State private var insetAmount: CGFloat = 50.0
     
     var body: some View {
-        VStack {
-            ZStack {
-                Circle()
-                    .fill(Color(red: 1, green: 0, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(Color(red: 0, green: 1, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: 50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(Color(red: 0, green: 0, blue: 1))
-                    .frame(width: 200 * amount)
-                    .blendMode(.screen)
-            }
-            .frame(width: 300, height: 300)
-            
-            Slider(value: $amount)
-                .padding()
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 200)
+            .onTapGesture {
+                withAnimation {
+                    self.insetAmount = CGFloat.random(in: 10...90)
+                }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black)
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
