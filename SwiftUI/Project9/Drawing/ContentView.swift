21b94b76fb20edeea417930c4fd61743d9891384
Anuj Rajput <anujrajput04@gmail.com>
2020-09-02 14:25:01 +0530

    Animating complex shapes with AnimatablePair

--- SwiftUI/Project9/Drawing/ContentView.swift
+++ SwiftUI/Project9/Drawing/ContentView.swift
@@ -8,36 +8,42 @@
 
 import SwiftUI
 
-struct Trapezoid: Shape {
-    var insetAmount: CGFloat
-    
-    var animatableData: CGFloat {
-        get { insetAmount }
-        set { self.insetAmount = newValue }
-    }
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
     
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.move(to: CGPoint(x: 0, y: rect.maxY))
-        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+        
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
         
         return path
     }
 }
 
 struct ContentView: View {
-    @State private var insetAmount: CGFloat = 50.0
+    @State private var rows = 4
+    @State private var columns = 4
     
     var body: some View {
-        Trapezoid(insetAmount: insetAmount)
-            .frame(width: 200, height: 200)
+        Checkerboard(rows: rows, columns: columns)
             .onTapGesture {
-                withAnimation {
-                    self.insetAmount = CGFloat.random(in: 10...90)
+                withAnimation(.linear(duration: 3)) {
+                    self.rows = 8
+                    self.columns = 16
                 }
         }
     }
