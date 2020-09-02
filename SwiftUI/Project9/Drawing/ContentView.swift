e2c659db6cee4239c792782b3f96c70825d397ee
Anuj Rajput <anujrajput04@gmail.com>
2020-09-02 13:47:36 +0530

    True RGB using Color(red:green:blue:) instead of what looks great on both Light and Dark mode

--- SwiftUI/Project9/Drawing/ContentView.swift
+++ SwiftUI/Project9/Drawing/ContentView.swift
@@ -15,19 +15,19 @@ struct ContentView: View {
         VStack {
             ZStack {
                 Circle()
-                    .fill(Color.red)
+                    .fill(Color(red: 1, green: 0, blue: 0))
                     .frame(width: 200 * amount)
                     .offset(x: -50, y: -80)
                     .blendMode(.screen)
                 
                 Circle()
-                    .fill(Color.green)
+                    .fill(Color(red: 0, green: 1, blue: 0))
                     .frame(width: 200 * amount)
                     .offset(x: 50, y: -80)
                     .blendMode(.screen)
                 
                 Circle()
-                    .fill(Color.blue)
+                    .fill(Color(red: 0, green: 0, blue: 1))
                     .frame(width: 200 * amount)
                     .blendMode(.screen)
             }
