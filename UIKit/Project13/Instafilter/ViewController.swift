31927ffd13cba3b302c3f0a67ca790b62d0be6b5
Anuj Rajput <anujrajput04@gmail.com>
2020-09-04 11:15:47 +0530

    Added Project 13: Instafilter Designing the interface

---
+++ UIKit/Project13/Instafilter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Anuj Rajput on 03/09/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+}
+
