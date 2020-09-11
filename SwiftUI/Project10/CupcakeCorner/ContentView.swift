152f664475cd424b7c460146bde67a02f263127f
Anuj Rajput <anujrajput04@gmail.com>
2020-09-11 11:34:22 +0530

    Created Project: CupcakeCorner Adding Codable conformance to @Published properties

---
+++ SwiftUI/Project10/CupcakeCorner/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Anuj Rajput on 10/09/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Paul Hudson"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
