3c0b1e732ea22e35fb54746848311cf485ea2b58
Anuj Rajput <me@anujrajput.com>
2020-09-17 17:06:00 +0530

    Added Project 17: Space Race

---
+++ UIKit/Project17/SpaceRace/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  SpaceRace
+//
+//  Created by Anuj Rajput on 17/09/20.
+//  Copyright © 2020 Anuj Rajput. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
