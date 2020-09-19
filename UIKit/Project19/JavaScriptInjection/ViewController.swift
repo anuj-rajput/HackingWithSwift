fc789c2993ed4c1cf7324430e13140a3ee06582b
Anuj Rajput <me@anujrajput.com>
2020-09-19 22:25:13 +0530

    Added Project 19: JavaScript Injection

---
+++ UIKit/Project19/JavaScriptInjection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JavaScriptInjection
+//
+//  Created by Anuj Rajput on 19/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
