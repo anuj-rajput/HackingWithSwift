e509230b3d4a81cab2c1cb7cb5c31758a3c5967d
Anuj Rajput <me@anujrajput.com>
2020-09-20 11:46:08 +0530

    Added Project 20: Fireworks Night

---
+++ UIKit/Project20/FireworksNight/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  FireworksNight
+//
+//  Created by Anuj Rajput on 20/09/20.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+    }
+}
