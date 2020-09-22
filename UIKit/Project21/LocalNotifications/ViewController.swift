5adfb19181ddb0977b51ec50152345c9d70740d2
Anuj Rajput <me@anujrajput.com>
2020-09-22 14:59:16 +0530

    Added Project 21: Local Notifications

---
+++ UIKit/Project21/LocalNotifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalNotifications
+//
+//  Created by Anuj Rajput on 22/09/20.
+//
+
+import UserNotifications
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
