158b6d31050d7476bfbd1680075eb7d3056b3f71
Anuj Rajput <me@anujrajput.com>
2020-09-25 18:09:38 +0530

    Added Project 13: Instafilter

---
+++ SwiftUI/Project13/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Anuj Rajput on 25/09/20.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
