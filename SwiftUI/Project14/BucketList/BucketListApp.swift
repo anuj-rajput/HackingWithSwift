f5d61758df5d4a5b19d2add55bc1425fcac919bd
Anuj Rajput <me@anujrajput.com>
2020-09-27 18:23:17 +0530

    Added Projec 14: Bucket List

---
+++ SwiftUI/Project14/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Anuj Rajput on 27/09/20.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
