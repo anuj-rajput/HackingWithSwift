e5cd019e42841e04fc4fc1f005430274f680143e
Anuj Rajput <me@anujrajput.com>
2020-09-29 20:20:05 +0530

    Advanced MKMapView with SwiftUI

--- SwiftUI/Project14/BucketList/ContentView.swift
+++ SwiftUI/Project14/BucketList/ContentView.swift
@@ -5,45 +5,41 @@
 //  Created by Anuj Rajput on 27/09/20.
 //
 
-import LocalAuthentication
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isAuthenticated = false
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
     
     var body: some View {
-        VStack {
-            if isAuthenticated {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
-            }
-        }
-        .onAppear(perform: authenticate)
-    }
-    
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-        
-        // check whether biometric authentication is possible or not
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            // it's possible so go ahead and use it
-            let reason = "We need to unlock your data"
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
             
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { (success, authenticatedError) in
-                // authentication is complete
-                DispatchQueue.main.async {
-                    if success {
-                        // authenticated successfully
-                        self.isAuthenticated = true
-                    } else {
-                        // there was a problem
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = MKPointAnnotation()
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
                     }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
                 }
             }
-        } else {
-            // no biometrics
         }
     }
 }
