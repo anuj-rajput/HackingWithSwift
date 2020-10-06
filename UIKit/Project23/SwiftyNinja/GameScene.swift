edd8f2a7d1150104fdbf6573309c8c5e601fbf94
Anuj Rajput <anujrajput04@gmail.com>
2020-10-06 11:30:44 +0530

    Added Project 23: Swifty Ninja

---
+++ UIKit/Project23/SwiftyNinja/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  SwiftyNinja
+//
+//  Created by Anuj Rajput on 06/10/20.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+        
+    override func didMove(to view: SKView) {
+    }
+}
